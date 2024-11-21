2008cc7b7d507130333b6e5a23396947e999d6aa
rishabh <rishabhksinghal@gmail.com>
2024-11-20 16:31:52 -0800

    no android

--- data.js
+++ data.js
@@ -69,24 +69,6 @@ export const projects = {
       techStack: ["Django", "SQLite", "Bootstrap", "JavaScript", "Heroku"],
     }
   ],
-  androidProjects: [
-    {
-      projectName: "NITW-CSE",
-      image: "images/nitwcse.jpg",
-      summary:
-        "The Application display details of Department courses, reference books, research, publication and faculty profile.",
-      preview: "https://github.com/vinaysomawat/NITW-CSE",
-      techStack: ["JAVA", "XML", "Android"],
-    },
-    {
-      projectName: "CareerHigh-App",
-      image: "images/carrerhigh.png",
-      summary:
-        "The Application display the webpages of website careerhigh.in in android devices.",
-      preview: "https://github.com/vinaysomawat/CareerHigh-Android",
-      techStack: ["JAVA", "XML", "Android"],
-    },
-  ],
   freelanceProjects: [
     {
       projectName: "SnylloAir.com",
